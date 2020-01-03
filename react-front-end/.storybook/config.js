f9b93a55463869d6afabb3a6bbb37eff167a7174
Zach Sullivan <zsullivan93@gmail.com>
2020-01-02 18:55:28 -0800

    initial storybook commit

---
+++ react-front-end/.storybook/config.js
@@ -0,0 +1,9 @@
+import { configure } from '@storybook/react';
+
+// automatically import all files ending in *.stories.js
+// configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+function loadStories() {
+  require('../src/stories');
+}
+
+configure(loadStories, module);
