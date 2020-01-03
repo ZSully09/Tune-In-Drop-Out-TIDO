d8451d87327f5cb98f2aa577235a8212a79c8afc
ZSully09 <zsullivan93@gmail.com>
2020-01-03 22:29:30 +0000

    added routes

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,36 +1,50 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import axios from 'axios';
+import Home from './components/Home';
+// import Create from './components/Create';
+// import Party from './components/Party';
+import Login from './components/Login';
+// import Register from './components/Register';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       message: 'Click the button to load data!'
-    }
+    };
   }
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+  // fetchData = () => {
+  //   axios
+  //     .get('/api/data') // You can simply make your requests to "/api/whatever you want"
+  //     .then(response => {
+  //       // handle success
+  //       console.log(response.data); // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
+  //       console.log(response.data.message); // Just the message
+  //       this.setState({
+  //         message: response.data.message
+  //       });
+  //     });
+  // };
 
   render() {
     return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          {/*<Route path="/register" component={Register} /> 
+          <Route path="/create" component={Create} />
+          <Route path="/party" component={Party} />
+          */}
+
+          {/* <h1>{this.state.message}</h1>
+          <button onClick={this.fetchData}>Fetch Data</button> */}
+        </div>
+      </Router>
     );
   }
 }
