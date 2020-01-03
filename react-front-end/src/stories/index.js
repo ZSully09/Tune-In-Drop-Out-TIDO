20e599bf5a7838614a58c6d7ee59b1b9fa930502
ZSully09 <zsullivan93@gmail.com>
2020-01-03 20:06:20 +0000

    story import adjustment

---
+++ react-front-end/src/stories/index.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+// import Button from 'components/Button.js';
+import CurrentSongPlaying from '../components/CurrentSongPlaying';
+import CurrentSongPaused from '../components/CurrentSongPaused';
+import CurrentSongNone from '../components/CurrentSongNone';
+
+// storiesOf('Button', module)
+//   .addParameters({
+//     backgrounds: [{ name: 'dark', value: '#222f3e', default: true }]
+//   })
+//   .add('Base', () => <Button>Base</Button>)
+//   .add('Confirm', () => <Button confirm>Save</Button>)
+//   .add('Danger', () => <Button danger>Cancel</Button>)
+//   .add('Clickable', () => (
+//     <Button onClick={action('button-clicked')}>Clickable</Button>
+//   ))
+//   .add('Disabled', () => (
+//     <Button disabled onClick={action('button-clicked')}>
+//       Disabled
+//     </Button>
+//   ));
+
+storiesOf('CurrentSong', module)
+  // .addParameters({
+  //   backgrounds: [{ name: 'dark', value: '#222f3e', default: true }]
+  // })
+  .add('Playing', () => <CurrentSongPlaying></CurrentSongPlaying>)
+  .add('Paused', () => <CurrentSongPaused></CurrentSongPaused>)
+  .add('None', () => <CurrentSongNone></CurrentSongNone>);
