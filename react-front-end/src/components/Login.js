9b67259c3dc10b4b2e1d2d2828302d89daa9ffac
Zach Sullivan <zsullivan93@gmail.com>
2020-01-03 18:05:59 -0800

    Login component

--- react-front-end/src/components/Login.js
+++ react-front-end/src/components/Login.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import classNames from 'classnames';
 import './Login.scss';
 
-const Login = () => (
-  <Grid container>
-    <Grid item style={{ margin: '0 auto' }}>
-      {/* <a
-                href="http://spotify-test-backend.herokuapp.com/login"
-                style={{ textDecoration: "none" }}
-            > */}
-      {/* prod */}
-      <a
-        href={`https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}&scope=${process.env.REACT_APP_SPOTIFY_SCOPE}&response_type=token`}
-        style={{ textDecoration: 'none' }}
-      >
-        <Typography variant="h1">Login with Spotify</Typography>
-      </a>
-    </Grid>
-  </Grid>
-);
-
-export default Login;
+export default function Login(props) {
+  const login = classNames('form--login');
+  const email = classNames('input--email');
+  const password = classNames('input--password');
+  const loginButton = classNames('button--login');
+  const loginViaSpotifyButton = classNames('button--spotify--login');
+  return (
+    <main>
+      <h3> TiDo Login </h3>
+      {/* Add action, method="POST" to the form */}
+      <form className={login}>
+        <input className={email} placeholder="Email"></input>
+        <input className={password} placeholder="Password"></input>
+      </form>
+      <button className={loginButton}> Login</button>
+      <button className={loginViaSpotifyButton}> Login with Spotify</button>
+    </main>
+  );
+}
