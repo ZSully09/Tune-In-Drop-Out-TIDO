7ca72683c1084dea0534e86aa4281ac37d0d110a
Nick Hoszko <nickhoszko@Nicks-MacBook-Pro.local>
2020-01-06 15:30:25 -0800

    search bar communicating with spotify

--- react-front-end/src/components/party_room/search_bar/LiveSearch.js
+++ react-front-end/src/components/party_room/search_bar/LiveSearch.js
@@ -2,20 +2,26 @@ import React, { Fragment, useState, useEffect } from 'react';
 import axios from "axios";
 
 import SearchBar from "./SearchBar";
-import Results from "./Results"
+import Results from "./Results";
 
 export default function LiveSearch(props) {
-  const [term, setTerm] = useState(""); // what we type in
-  const [results, setResults] = useState([]); // the results list
-  console.log(term);
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
   useEffect(() => {
-    axios(`	https://api.spotify.com/v1/search?term=${term} `)
+    console.log('search',term);
+    // hardcoded for test, eventually comes from db
+    const token = 'BQAeMZgcO8E7RDbjOxJu-VxvgfFwDbaBpXVkTYuOA-HkVE7ZvzwKarjxMzyF8QpCg8LIquGDicTLOX4NabwU8WjR7-KuvnOlQ5Wmvl6kIrPG95wXqeX1dn68X6vTDpFUBxU2O8nXFzkz655jFIKP4vnhKzYa4DY2GJg1MTEWrRCM1O0ThB0kfJg'
+    axios(`	https://api.spotify.com/v1/search?q=name:${term}&type=track `, 
+    {
+      headers: {Authorization: `Bearer ${token}`}
+    }).then(res => console.log(term))
+    .catch(err => console.log(err))
   }, [term]);
+
 
   return (
     <Fragment>
       <SearchBar setTerm={setTerm} term={term}>
-        
       </SearchBar>
     </Fragment>  
   );
