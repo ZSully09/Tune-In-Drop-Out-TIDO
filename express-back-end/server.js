6b68942357536cf5b2bc1c33c2600cb1f8fe6218
drystar <chris.drysdale12@gmail.com>
2020-01-06 17:51:05 -0800

    create db

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,18 +1,28 @@
-const Express = require('express');
+const Express = require("express");
 const App = Express();
-const BodyParser = require('body-parser');
+const BodyParser = require("body-parser");
 const PORT = 8080;
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
-App.use(Express.static('public'));
+App.use(Express.static("public"));
+
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
 
 // Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
+App.get("/api/data", (req, res) =>
+  res.json({
+    message: "Seems to work!"
+  })
+);
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  console.log(
+    `Express seems to be listening on port ${PORT} so that's pretty good 👍`
+  );
 });
