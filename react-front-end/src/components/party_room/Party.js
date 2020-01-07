1ab45b7dec919a3c17f1d86129a671989e174cb8
Zach Sullivan <zsullivan93@gmail.com>
2020-01-06 18:14:36 -0800

    adjusted playlist and party

--- react-front-end/src/components/party_room/Party.js
+++ react-front-end/src/components/party_room/Party.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
+import Header from './header/Header';
+import CurrentSongNone from './current_song/CurrentSongNone';
+import CurrentSongPaused from './current_song/CurrentSongPaused';
+import CurrentSongPlaying from './current_song/CurrentSongPlaying';
+import Song from './playlist/song/Song';
 import './Party.scss';
 
 export default function Party(props) {
   return (
     <main>
-      <h3>Party</h3>
+      <Header />
+      <div>
+        <Song />
+      </div>
+      <footer>
+        {/* IF SONG IS PLAYING */}
+        <CurrentSongPlaying />
+        {/* IF SONG IS PAUSED 
+        <CurrentSongPaused /> */}
+        {/* IF THERE IS NO CURRENT SONG
+        <CurrentSongNone /> */}
+      </footer>
     </main>
   );
 }
