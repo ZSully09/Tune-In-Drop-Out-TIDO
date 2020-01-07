9b7fedb5fc666f002e2646f0b6f82f187cbe0cb0
drystar <chris.drysdale12@gmail.com>
2020-01-07 11:44:33 -0800

    creating user query helpers

--- database/db.js
+++ database/db.js
@@ -1,4 +1,5 @@
 //add new user to db
+const bcrypt = require("bcrypt");
 
 const addUser = function(user, db) {
   return db
@@ -13,4 +14,22 @@ const addUser = function(user, db) {
         return data.rows[0];
       }
     });
+};
+
+// search for user by email
+const getUserByEmail = function(email, db) {
+  return db.query(
+    `
+  SELECT * FROM users WHERE email=$1`,
+    [`${email}`]
+  );
+};
+
+// Get user by client_id used to show a users email in the header.
+const getUserById = function(client_id, db) {
+  return db.query(
+    `
+  SELECT * FROM users WHERE client_id=$1`,
+    [`${id}`]
+  );
 };
---
+++ database/queries/query_users.js
@@ -0,0 +1,32 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+// ================== GET ROUTES =======================
+
+// GET ALL USERS
+
+const getUsers = (request, response) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+// GET SINGLE USER
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
