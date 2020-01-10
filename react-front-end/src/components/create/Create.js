1dda7274b8efa01a42e56d895d6041e7df0fe831
drystar <chris.drysdale12@gmail.com>
2020-01-10 14:22:08 -0800

    create new party [hard coded]

--- react-front-end/src/components/create/Create.js
+++ react-front-end/src/components/create/Create.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import classNames from 'classnames';
-import './Create.scss';
+import React from "react";
+import { Redirect, Link } from "react-router-dom";
+import { chance } from "chance";
+
+import classNames from "classnames";
+import "./Create.scss";
+import Axios from "axios";
 
 export default function Create(props) {
-  const createPartyForm = classNames('formn--party');
-  const partyName = classNames('input--party--name');
-  const partyAccessCode = classNames('input--Access--Code');
-  const createNewPartyButton = classNames('button--create--new');
+  // const createPartyForm = classNames("form--party");
+  // // const partyName = classNames("input--party--name");
+
+  const partyName = () => {
+    return Axios.post("/api/party", { partyName: "12345s" });
+  };
+
+  // random partyName generator
+  // const partyName = chance.sentence({ words: 3 });
+  // console.log("partyName", partyName);
+
+  const partyAccessCode = classNames("input--Access--Code");
+  const createNewPartyButton = classNames("button--create--new");
   return (
     <main>
       <h3> Create a New Party </h3>
-      <form className={createPartyForm}>
-        <input className={partyName} placeholder="Party Name"></input>
-        <input className={partyAccessCode} placeholder="Access Code"></input>
+      <form
+        className={createPartyForm}
+        action="/api/party"
+        method="post"
+        name="createNewPartyButton"
+      >
+        <input
+          type="partyName"
+          className={partyName}
+          placeholder="Party Name"
+        ></input>
         <Link to="/party">
           <button className={createNewPartyButton}> New Party </button>
         </Link>
