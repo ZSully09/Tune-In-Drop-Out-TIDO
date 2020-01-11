77aa6db367fdc1aea0f2ad85064c2c06cfc11ccf
Zach Sullivan <zsullivan93@gmail.com>
2020-01-11 15:26:26 -0800

    Create Party adjustments

--- react-front-end/src/components/create/Create.js
+++ react-front-end/src/components/create/Create.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
+import makePartyName from '../../helpers/names';
 import './Create.scss';
 
 export default function Create(props) {
   const createPartyForm = classNames('formn--party');
   const partyName = classNames('input--party--name');
-  const partyAccessCode = classNames('input--Access--Code');
   const createNewPartyButton = classNames('button--create--new');
   return (
     <main>
       <h3> Create a New Party </h3>
       <form className={createPartyForm}>
-        <input className={partyName} placeholder="Party Name"></input>
-        <input className={partyAccessCode} placeholder="Access Code"></input>
+        <input className={partyName} placeholder={makePartyName()}></input>
         <Link to="/party">
           <button className={createNewPartyButton}> New Party </button>
         </Link>
