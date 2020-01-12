6a54c3af005e8661a382e6ba9d3ff061de33e154
Zach Sullivan <zsullivan93@gmail.com>
2020-01-12 10:53:57 -0800

    Create and join party adjustments

--- react-front-end/src/components/create/Create.js
+++ react-front-end/src/components/create/Create.js
@@ -8,13 +8,30 @@ export default function Create(props) {
   const createPartyForm = classNames('formn--party');
   const partyName = classNames('input--party--name');
   const createNewPartyButton = classNames('button--create--new');
+
+  // const setParty = () => {
+  //   const party = {
+  //     partyName: props.makePartyName
+  //   };
+  //   addToast(`${song.songName} was added to the playlist`, {
+  //     appearance: 'success',
+  //     autoDismiss: true
+  //   });
+  //   console.log(song);
+
+  //   // TODO: call some callback that came in from props
+  //   props.onSelectSong(song);
+  // };
+
   return (
     <main>
       <h3> Create a New Party </h3>
       <form className={createPartyForm}>
-        <input className={partyName} placeholder={makePartyName()}></input>
+        <input className={partyName} type="text">
+          {props.partyName}
+        </input>
         <Link to="/party">
-          <button className={partyName}> New Party </button>
+          <button className={createNewPartyButton}> New Party </button>
         </Link>
       </form>
     </main>
--- react-front-end/src/components/join/Join.js
+++ react-front-end/src/components/join/Join.js
@@ -6,14 +6,12 @@ import './Join.scss';
 export default function Join(props) {
   const createPartyForm = classNames('formn--party');
   const partyName = classNames('input--party--name');
-  const partyAccessCode = classNames('input--Access--Code');
   const createNewPartyButton = classNames('button--create--new');
   return (
     <main>
       <h3> Join a Party </h3>
       <form className={createPartyForm}>
         <input className={partyName} placeholder="Party Name"></input>
-        <input className={partyAccessCode} placeholder="Access Code"></input>
         <Link to="/party">
           <button className={createNewPartyButton}> New Party </button>
         </Link>
